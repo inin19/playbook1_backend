5871ac61b67489008a7be5c213f66944712ee510
Chen, Ying Ying <Ying-Ying.Chen@marsh.com>
2019-07-01 10:18:21 -0400

    ignore build files in git

--- app.js
+++ app.js
@@ -6,18 +6,31 @@ const path = require('path');
 const dotenv = require('dotenv');
 const redis = require('redis');
 const { promisify } = require('util');
+const compresssion = require('compression');
+const bodyParser = require('body-parser');
+
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
+
 const retryStrategy = require('./config/redisRetry');
 
-// const compresssion = require('compression');
+const allowedExt = [
+  '.js',
+  '.ico',
+  '.css',
+  '.png',
+  '.jpg',
+  '.woff2',
+  '.woff',
+  '.ttf',
+  '.svg',
+];
 
 process.env.ENV = process.argv[2] || 'dev';
 
 const logger = require('./config/winston');
 
 dotenv.config({ path: path.resolve(process.cwd(), `${process.env.ENV}.env`) });
-
-const port = 3100;
-const app = express();
 
 
 const client = redis.createClient({
@@ -35,17 +48,43 @@ client.on('error', (err) => {
   logger.error('Redis connection failed');
 });
 
+const port = 3100;
+const app = express();
 
+app.use(compresssion());
 app.use(morgan('tiny', { stream: logger.stream }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.get('/', (req, res) => {
+//   res.json({ name: 'yingying' });
+// });
+
+// app.get('*', (req, res, next) => {
+//   Promise.resolve().then(() => {
+//     throw new Error('BROKEN');
+//   }).catch(next);
+// });
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  if (allowedExt.includes(req.url)) {
+    res.sendFile(path.resolve(`public/${req.url}`));
+  } else {
+    res.sendFile(path.resolve('public/index.html'));
+  }
 });
+
+// app.use((err, req, res, next) => {
+//   logger.error(err.stack);
+//   res.status(500).send('Something broke!');
+// });
 
 
 app.use((err, req, res, next) => {
   res.locals.message = err.message;
-  // res.locals.error = req.app.get('env') === 'dev' ? err : {};
+  res.locals.error = req.app.get('env') === 'dev' ? err : {};
 
 
   // add this line to include winston logging
@@ -53,7 +92,8 @@ app.use((err, req, res, next) => {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  // res.render('error');
+  res.json({ name: 'error' });
 });
 
 
